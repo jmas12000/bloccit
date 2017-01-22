1dd69e26a36e1cb1d8036374d6ed17f9a6f8787a
Jeff Moore <jmas1_2000@hotmail.com>
2017-01-22 22:04:41 +0000

    assignment 17 Seeding Data: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,10 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
+
+puts "Number of posts: #{Post.count}"
+Post.find_or_create_by(title: "The only on of this title", body: "Test body")
+puts "Number of posts after method: #{Post.count}"
  
 puts "Seed finished"
 puts "#{Post.count} posts created"
