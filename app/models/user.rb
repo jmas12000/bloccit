fa59365762b11e0a68e083c40a69474dc9a08985
Jeff Moore <jmas1_2000@hotmail.com>
2017-02-04 05:24:05 +0000

    assignment 24 Authentication User Model: completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase if email.present? }
-
+  before_save { email.downcase if email.present?  }
+  before_save :correct_name
+ 
+  def correct_name
+    names = []
+    names << name.split(' ').map(&:capitalize)
+    self.name = names.join(' ') 
+  end
+  
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password")}
+  
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
- 
+
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_least(3) }
@@ -17,6 +18,14 @@ RSpec.describe User, type: :model do
   describe "attributes" do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
+    end
+  end
+  
+  describe "proper name capitalization" do
+    it "should fix lower-case name entries" do
+      user.name = "jeff moore"
+      user.save 
+      expect(user.name).to eq "Jeff Moore"
     end
   end
   
