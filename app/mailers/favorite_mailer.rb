42412fc46c4e503fabcdc836c1f1f8abba72182a
Jeff Moore <jmas1_2000@hotmail.com>
2017-02-15 07:22:13 +0000

    assignment 31 Favorites: completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,7 +2,6 @@ class FavoriteMailer < ApplicationMailer
   default from: "jmas1_2000@hotmail.com"
   
   def new_comment(user, post, comment)
- 
     headers["Message-ID"] = "<comments/#{comment.id}@shielded-temple-33287.herokuapp.com>"
     headers["In-Reply-To"] = "<post/#{post.id}@shielded-temple-33287.herokuapp.com>"
     headers["References"] = "<post/#{post.id}@shielded-temple-33287.herokuapp.com>"
@@ -12,5 +11,15 @@ class FavoriteMailer < ApplicationMailer
     @comment = comment
  
     mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+  
+  def new_post(post) 
+    headers["Message-ID"] = "<posts/#{post.id}@shielded-temple-33287.herokuapp.com>"
+    headers["In-Reply-To"] = "<posts/#{post.id}@shielded-temple-33287.herokuapp.com>"
+    headers["References"] = "<posts/#{post.id}@shielded-temple-33287.herokuapp.com>"
+ 
+    @post = post
+ 
+    mail(to: post.user.email, subject: "New favorite on #{post.title}")
   end
 end
